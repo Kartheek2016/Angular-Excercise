cb34f52c66d6c562c2cea575a2ef26761c1ff127
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-25 09:45:12 +0530

    Invoked service

--- pms/js/models/ProService.js
+++ pms/js/models/ProService.js
@@ -12,36 +12,37 @@ app.factory('MainService', function ($http) {
 	};
 });
 
-app.factory('UserService', function ($http) {
+app.factory('UserService', function ($http, MainService) {
 	return {
 		get: function () {
-			return $http.get('../js/modules/users.json');
+			return MainService.getUsers();
 		}
 	}
 });
 
-app.factory('ProjectService', function ($http) {
+app.factory('ProjectService', function ($http, MainService) {
 	return {
 		get: function () {
-			return $http.get('../js/modules/projects.json');
+			return MainService.getProjects();
 		}
 	}
 });
 
-app.factory('TaskService', function ($http) {
+app.factory('TaskService', function ($http, MainService) {
 	return {
 		get: function () {
-			return $http.get('../js/modules/tasks.json');
+			return MainService.getTasks();
 		}
 	}
 });
 
-app.factory('UserProjectsService', function ($http, $q) {
+app.factory('UserProjectsService', function ($http, $q, MainService) {
 	return {
 		getProject: function (user_id) {
 			var defered = $q.defer();
 			var particularProject = [];
-			$http.get('../js/modules/projects.json').success(function(data) {
+			MainService.getProjects().success(function(data) {
+				console.log(data);
 				for(var i = 0; i < data.projects.length; i++) {
 					for(var j = 0; j < data.projects[i].project_members.user_id.length; j++) {
 						if(user_id == data.projects[i].project_members.user_id[j]){
